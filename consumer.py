2ca9811636aef7855def70c540af086ec4ab18e6
Jakub Wszolek <Jakub.Wszolek@acxiom.com>
2022-04-04 14:51:28 +0200

    initial commit

---
+++ consumer.py
@@ -0,0 +1,26 @@
+import boto3
+import json
+from datetime import datetime
+import time
+
+my_stream_name = 'test1'
+
+boto3.setup_default_session(profile_name='aws-edu')
+kinesis_client = boto3.client('kinesis', region_name='us-east-1')
+response = kinesis_client.describe_stream(StreamName=my_stream_name)
+
+print(response['StreamDescription']['Shards'])
+my_shard_id = response['StreamDescription']['Shards'][0]['ShardId']
+print(my_shard_id)
+
+my_shard_id = "shardId-000000000001"
+shard_iterator = kinesis_client.get_shard_iterator(StreamName=my_stream_name,ShardId=my_shard_id,ShardIteratorType='LATEST')
+my_shard_iterator = shard_iterator['ShardIterator']
+record_response = kinesis_client.get_records(ShardIterator=my_shard_iterator, Limit=2)
+
+while 'NextShardIterator' in record_response:
+    record_response = kinesis_client.get_records(ShardIterator=record_response['NextShardIterator'],Limit=2)
+    print(record_response)
+
+    # wait for 5 seconds
+    time.sleep(5)
